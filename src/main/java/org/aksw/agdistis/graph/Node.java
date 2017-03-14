85189ef68e8e36f740fb4bfea2699120465f020b
Giorgio Orsi <orsi.giorgio@gmail.com>
2017-03-14 13:43:21 +0000

    Hotfixes

--- src/main/java/org/aksw/agdistis/graph/Node.java
+++ src/main/java/org/aksw/agdistis/graph/Node.java
@@ -1,247 +1,253 @@
 package org.aksw.agdistis.graph;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.util.HashSet;
 import java.util.Properties;
 
 public class Node implements Comparable<Node> {
 
-	private HashSet<Integer> ids;
-	private double activation;
-	private String candidateURI;
-	private int level;
-	private double hubWeightForCalculation = 1;
-	private double authorityWeightForCalculation = 1;
-	private double unnormalizedHubWeight;
-	private double unnormalizedAuthorityWeight;
-	private double hubWeight;
-	private double authorityWeight;
-	private double pageRank;
-	private double pageRankNew;
-	private String algorithm;
+  private final HashSet<Integer> ids;
+  private double activation;
+  private String candidateURI;
+  private int level;
+  private double hubWeightForCalculation = 1;
+  private double authorityWeightForCalculation = 1;
+  private double unnormalizedHubWeight;
+  private double unnormalizedAuthorityWeight;
+  private double hubWeight;
+  private double authorityWeight;
+  private double pageRank;
+  private double pageRankNew;
+  private final static Properties prop = new Properties();
+  private static String algorithm;
+  private final static String _DEFAULT_ALGORITHM = "hits";
 
-	private HashSet<Node> predecessors;
-	private HashSet<Node> successors;
+  static {
+    try {
+      prop.load(Node.class.getResourceAsStream("/config/agdistis.properties"));
+      algorithm = prop.getProperty("algorithm");
+    } catch (final IOException ioe) {
+      algorithm = _DEFAULT_ALGORITHM;
+    }
+  }
 
-	public Node(String uri, double activation, int level) throws IOException {
-		this.candidateURI = uri;
-		this.activation = activation;
-		this.level = level;
-		this.hubWeight = 1;
-		this.authorityWeight = 1;
-		ids = new HashSet<Integer>();
-		this.successors = new HashSet<Node>();
-		this.predecessors = new HashSet<Node>();
-		this.pageRank = 0;
-		Properties prop = new Properties();
-		InputStream input = Node.class.getResourceAsStream("/config/agdistis.properties");
-		prop.load(input);
-		this.algorithm = prop.getProperty("algorithm");
-	}
+  private final HashSet<Node> predecessors;
+  private final HashSet<Node> successors;
 
-	@Override
-	public String toString() {
-		DecimalFormat df = new DecimalFormat("#.####");
-		return candidateURI + ":" + String.valueOf(df.format(activation)) + " H: " + String.valueOf(df.format(hubWeight)
-				+ " A: " + String.valueOf(df.format(authorityWeight) + " PR: " + String.valueOf(df.format(pageRank))));
-	}
+  public Node(final String uri, final double activation, final int level) throws IOException {
+    candidateURI = uri;
+    this.activation = activation;
+    this.level = level;
+    hubWeight = 1;
+    authorityWeight = 1;
+    ids = new HashSet<Integer>();
+    successors = new HashSet<Node>();
+    predecessors = new HashSet<Node>();
+    pageRank = 0;
+  }
 
-	@Override
-	public int hashCode() {
-		return candidateURI.hashCode();
-	}
+  @Override
+  public String toString() {
+    final DecimalFormat df = new DecimalFormat("#.####");
+    return candidateURI + ":" + String.valueOf(df.format(activation)) + " H: " + String.valueOf(df.format(hubWeight)
+        + " A: " + String.valueOf(df.format(authorityWeight) + " PR: " + String.valueOf(df.format(pageRank))));
+  }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this.hashCode() == o.hashCode()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+  @Override
+  public int hashCode() {
+    return candidateURI.hashCode();
+  }
 
-	@Override
-	// this determines the ordering of candidates and thus the result of the
-	// algorithm
-	// change to hub score
-	public int compareTo(Node m) {
+  @Override
+  public boolean equals(final Object o) {
+    if (hashCode() == o.hashCode()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-		if (algorithm.equals("hits")) {
-			// System.out.println("AuthorityWeight");
-			if (m.getAuthorityWeight() == this.getAuthorityWeight()) {
-				return 0;
-			} else if (m.getAuthorityWeight() > this.getAuthorityWeight()) {
-				return 1;
-			} else {
-				return -1;
-			}
-		} else if (algorithm.equals("pagerank")) {
-			// System.out.println("PageRank compareTo");
-			if (m.getPageRank() == this.getPageRank()) {
-				return 0;
-			} else if (m.getPageRank() > this.getPageRank()) {
-				return 1;
-			} else {
-				return -1;
-			}
-		} else {
-			return -1;
-		}
-		// HubWeight
-		// if (m.getHubWeight() == this.getHubWeight()) {
-		// return 0;
-		// } else if (m.getHubWeight() > this.getHubWeight()) {
-		// return 1;
-		// } else {
-		// return -1;
-		// }
+  @Override
+  // this determines the ordering of candidates and thus the result of the
+  // algorithm
+  // change to hub score
+  public int compareTo(final Node m) {
 
-		// AuthorityWeight && PageRank
-		// if (m.getPageRank() * (2*(m.getAuthorityWeight() + m.getHubWeight()))
-		// == this.getPageRank() * (2*(this.getAuthorityWeight() +
-		// this.getHubWeight()))) {
-		// return 0;
-		// } else if (m.getPageRank() * (2*(m.getAuthorityWeight() +
-		// m.getHubWeight())) > this.getPageRank() *
-		// (2*(this.getAuthorityWeight() + this.getHubWeight()))) {
-		// return 1;
-		// } else {
-		// return -1;
-		// }
-	}
+    if (algorithm.equals("hits")) {
+      // System.out.println("AuthorityWeight");
+      if (m.getAuthorityWeight() == getAuthorityWeight()) {
+        return 0;
+      } else if (m.getAuthorityWeight() > getAuthorityWeight()) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else if (algorithm.equals("pagerank")) {
+      // System.out.println("PageRank compareTo");
+      if (m.getPageRank() == getPageRank()) {
+        return 0;
+      } else if (m.getPageRank() > getPageRank()) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else {
+      return -1;
+    }
+    // HubWeight
+    // if (m.getHubWeight() == this.getHubWeight()) {
+    // return 0;
+    // } else if (m.getHubWeight() > this.getHubWeight()) {
+    // return 1;
+    // } else {
+    // return -1;
+    // }
 
-	public boolean containsId(int id) {
-		return ids.contains(id);
-	}
+    // AuthorityWeight && PageRank
+    // if (m.getPageRank() * (2*(m.getAuthorityWeight() + m.getHubWeight()))
+    // == this.getPageRank() * (2*(this.getAuthorityWeight() +
+    // this.getHubWeight()))) {
+    // return 0;
+    // } else if (m.getPageRank() * (2*(m.getAuthorityWeight() +
+    // m.getHubWeight())) > this.getPageRank() *
+    // (2*(this.getAuthorityWeight() + this.getHubWeight()))) {
+    // return 1;
+    // } else {
+    // return -1;
+    // }
+  }
 
-	public void addId(int id) {
-		ids.add(id);
-	}
+  public boolean containsId(final int id) {
+    return ids.contains(id);
+  }
 
-	public HashSet<Integer> getId() {
-		return ids;
-	}
+  public void addId(final int id) {
+    ids.add(id);
+  }
 
-	public String getCandidateURI() {
-		return candidateURI;
-	}
+  public HashSet<Integer> getId() {
+    return ids;
+  }
 
-	public void setCandidateURI(String uri) {
-		this.candidateURI = uri;
-	}
+  public String getCandidateURI() {
+    return candidateURI;
+  }
 
-	public void setActivation(double activation) {
-		this.activation = activation;
+  public void setCandidateURI(final String uri) {
+    candidateURI = uri;
+  }
 
-	}
+  public void setActivation(final double activation) {
+    this.activation = activation;
 
-	public double getActivation() {
-		return activation;
-	}
+  }
 
-	public int getLevel() {
-		return level;
-	}
+  public double getActivation() {
+    return activation;
+  }
 
-	public void setLevel(int i) {
-		level = i;
+  public int getLevel() {
+    return level;
+  }
 
-	}
+  public void setLevel(final int i) {
+    level = i;
 
-	public double getHubWeight() {
-		return hubWeight;
-	}
+  }
 
-	public void setAuthorityWeight(double x) {
-		this.authorityWeight = x;
+  public double getHubWeight() {
+    return hubWeight;
+  }
 
-	}
+  public void setAuthorityWeight(final double x) {
+    authorityWeight = x;
 
-	public double getAuthorityWeight() {
-		return authorityWeight;
-	}
+  }
 
-	public void setHubWeight(double y) {
-		this.hubWeight = y;
+  public double getAuthorityWeight() {
+    return authorityWeight;
+  }
 
-	}
+  public void setHubWeight(final double y) {
+    hubWeight = y;
 
-	public Integer[] getLabels() {
-		return ids.toArray(new Integer[ids.size()]);
-	}
+  }
 
-	public HashSet<Node> getSuccessors() {
-		return successors;
-	}
+  public Integer[] getLabels() {
+    return ids.toArray(new Integer[ids.size()]);
+  }
 
-	public HashSet<Node> getPredecessors() {
-		return predecessors;
-	}
+  public HashSet<Node> getSuccessors() {
+    return successors;
+  }
 
-	public void addPredecessor(Node first) {
-		predecessors.add(first);
-	}
+  public HashSet<Node> getPredecessors() {
+    return predecessors;
+  }
 
-	public void addSuccesor(Node second) {
-		successors.add(second);
-	}
+  public void addPredecessor(final Node first) {
+    predecessors.add(first);
+  }
 
-	public void removeSuccesor(Node right) {
-		successors.remove(right);
-	}
+  public void addSuccesor(final Node second) {
+    successors.add(second);
+  }
 
-	public void removePredecessor(Node left) {
-		predecessors.remove(left);
-	}
+  public void removeSuccesor(final Node right) {
+    successors.remove(right);
+  }
 
-	public double getHubWeightForCalculation() {
-		return hubWeightForCalculation;
-	}
+  public void removePredecessor(final Node left) {
+    predecessors.remove(left);
+  }
 
-	public void setHubWeightForCalculation(double hubWeightForCalculation) {
-		this.hubWeightForCalculation = hubWeightForCalculation;
-	}
+  public double getHubWeightForCalculation() {
+    return hubWeightForCalculation;
+  }
 
-	public double getAuthorityWeightForCalculation() {
-		return authorityWeightForCalculation;
-	}
+  public void setHubWeightForCalculation(final double hubWeightForCalculation) {
+    this.hubWeightForCalculation = hubWeightForCalculation;
+  }
 
-	public void setAuthorityWeightForCalculation(double authorityWeightForCalculation) {
-		this.authorityWeightForCalculation = authorityWeightForCalculation;
-	}
+  public double getAuthorityWeightForCalculation() {
+    return authorityWeightForCalculation;
+  }
 
-	public double getUnnormalizedHubWeight() {
-		return unnormalizedHubWeight;
-	}
+  public void setAuthorityWeightForCalculation(final double authorityWeightForCalculation) {
+    this.authorityWeightForCalculation = authorityWeightForCalculation;
+  }
 
-	public void setUnnormalizedHubWeight(double unnormalizedHubWeight) {
-		this.unnormalizedHubWeight = unnormalizedHubWeight;
-	}
+  public double getUnnormalizedHubWeight() {
+    return unnormalizedHubWeight;
+  }
 
-	public double getUnnormalizedAuthorityWeight() {
-		return unnormalizedAuthorityWeight;
-	}
+  public void setUnnormalizedHubWeight(final double unnormalizedHubWeight) {
+    this.unnormalizedHubWeight = unnormalizedHubWeight;
+  }
 
-	public void setUnnormalizedAuthorityWeight(double unnormalizedAuthorityWeight) {
-		this.unnormalizedAuthorityWeight = unnormalizedAuthorityWeight;
-	}
+  public double getUnnormalizedAuthorityWeight() {
+    return unnormalizedAuthorityWeight;
+  }
 
-	public double getPageRank() {
-		return pageRank;
-	}
+  public void setUnnormalizedAuthorityWeight(final double unnormalizedAuthorityWeight) {
+    this.unnormalizedAuthorityWeight = unnormalizedAuthorityWeight;
+  }
 
-	public void setPageRank(double pageRank) {
-		this.pageRank = pageRank;
-	}
+  public double getPageRank() {
+    return pageRank;
+  }
 
-	public double getPageRankNew() {
-		return pageRankNew;
-	}
+  public void setPageRank(final double pageRank) {
+    this.pageRank = pageRank;
+  }
 
-	public void setPageRankNew(double pageRankNew) {
-		this.pageRankNew = pageRankNew;
-	}
+  public double getPageRankNew() {
+    return pageRankNew;
+  }
+
+  public void setPageRankNew(final double pageRankNew) {
+    this.pageRankNew = pageRankNew;
+  }
 
 }
--- src/test/java/AGDISTISTest.java
+++ src/test/java/AGDISTISTest.java
@@ -13,104 +13,136 @@ import org.junit.Test;
 
 public class AGDISTISTest {
 
-	@Test
-	public void testUmlaute() throws InterruptedException, IOException {
-		String taisho = "Emperor Taishō";
-		String taishoURL = "http://dbpedia.org/resource/Emperor_Taishō";
-		String japan = "Japan";
-		String japanURL = "http://dbpedia.org/resource/Japan";
+  @Test
+  public void testUmlaute() throws InterruptedException, IOException {
+    final String taisho = "Emperor Taishō";
+    final String taishoURL = "http://dbpedia.org/resource/Emperor_Taishō";
+    final String japan = "Japan";
+    final String japanURL = "http://dbpedia.org/resource/Japan";
 
-		HashMap<String, String> correct = new HashMap<String, String>();
-		correct.put(taisho, taishoURL);
-		correct.put(japan, japanURL);
-		String preAnnotatedText = "<entity>" + taisho + "</entity> was the 123rd Emperor of <entity>" + japan
-				+ "</entity>.";
+    final HashMap<String, String> correct = new HashMap<String, String>();
+    correct.put(taisho, taishoURL);
+    correct.put(japan, japanURL);
+    final String preAnnotatedText = "<entity>" + taisho + "</entity> was the 123rd Emperor of <entity>" + japan
+        + "</entity>.";
 
-		NEDAlgo_HITS agdistis = new NEDAlgo_HITS();
-		Document d = GetDisambiguation.textToDocument(preAnnotatedText);
-		agdistis.run(d, null);
+    final NEDAlgo_HITS agdistis = new NEDAlgo_HITS();
+    final Document d = GetDisambiguation.textToDocument(preAnnotatedText);
+    agdistis.run(d, null);
 
-		NamedEntitiesInText namedEntities = d.getNamedEntitiesInText();
-		HashMap<NamedEntityInText, String> results = new HashMap<NamedEntityInText, String>();
-		for (NamedEntityInText namedEntity : namedEntities) {
-			String disambiguatedURL = namedEntity.getNamedEntityUri();
-			results.put(namedEntity, disambiguatedURL);
-		}
-		for (NamedEntityInText namedEntity : namedEntities) {
-			String disambiguatedURL = namedEntity.getNamedEntityUri();
-			System.out.println(namedEntity.getLabel() + " -> " + disambiguatedURL);
-			assertTrue(correct.get(namedEntity.getLabel()).equals(disambiguatedURL));
-		}
+    final NamedEntitiesInText namedEntities = d.getNamedEntitiesInText();
+    final HashMap<NamedEntityInText, String> results = new HashMap<NamedEntityInText, String>();
+    for (final NamedEntityInText namedEntity : namedEntities) {
+      final String disambiguatedURL = namedEntity.getNamedEntityUri();
+      results.put(namedEntity, disambiguatedURL);
+    }
+    for (final NamedEntityInText namedEntity : namedEntities) {
+      final String disambiguatedURL = namedEntity.getNamedEntityUri();
+      System.out.println(namedEntity.getLabel() + " -> " + disambiguatedURL);
+      assertTrue(correct.get(namedEntity.getLabel()).equals(disambiguatedURL));
+    }
 
-	}
+  }
 
-	@Test
-	public void testMinimalExample() throws InterruptedException, IOException {
-		String obama = "Barack Obama";
-		String obamaURL = "http://dbpedia.org/resource/Barack_Obama";
-		String merkel = "Angela Merkel";
-		String merkelURL = "http://dbpedia.org/resource/Angela_Merkel";
-		String city = "Berlin";
-		String cityURL = "http://dbpedia.org/resource/Berlin";
+  @Test
+  public void testMinimalExample() throws InterruptedException, IOException {
+    final String obama = "Barack Obama";
+    final String obamaURL = "http://dbpedia.org/resource/Barack_Obama";
+    final String merkel = "Angela Merkel";
+    final String merkelURL = "http://dbpedia.org/resource/Angela_Merkel";
+    final String city = "Berlin";
+    final String cityURL = "http://dbpedia.org/resource/Berlin";
 
-		HashMap<String, String> correct = new HashMap<String, String>();
-		correct.put(obama, obamaURL);
-		correct.put(merkel, merkelURL);
-		correct.put(city, cityURL);
+    final HashMap<String, String> correct = new HashMap<String, String>();
+    correct.put(obama, obamaURL);
+    correct.put(merkel, merkelURL);
+    correct.put(city, cityURL);
 
-		String preAnnotatedText = "<entity>" + obama + "</entity> visits <entity>" + merkel + "</entity> in <entity>"
-				+ city + "</entity>.";
+    final String preAnnotatedText = "<entity>" + obama + "</entity> visits <entity>" + merkel + "</entity> in <entity>"
+        + city + "</entity>.";
 
-		NEDAlgo_HITS agdistis = new NEDAlgo_HITS();
-		Document d = GetDisambiguation.textToDocument(preAnnotatedText);
-		agdistis.run(d, null);
+    final NEDAlgo_HITS agdistis = new NEDAlgo_HITS();
+    final Document d = GetDisambiguation.textToDocument(preAnnotatedText);
+    agdistis.run(d, null);
 
-		NamedEntitiesInText namedEntities = d.getNamedEntitiesInText();
-		HashMap<NamedEntityInText, String> results = new HashMap<NamedEntityInText, String>();
-		for (NamedEntityInText namedEntity : namedEntities) {
-			String disambiguatedURL = namedEntity.getNamedEntityUri();
-			results.put(namedEntity, disambiguatedURL);
-		}
-		for (NamedEntityInText namedEntity : namedEntities) {
-			String disambiguatedURL = namedEntity.getNamedEntityUri();
-			System.out.println(namedEntity.getLabel() + " -> " + disambiguatedURL);
-			assertTrue(correct.get(namedEntity.getLabel()).equals(disambiguatedURL));
-		}
+    final NamedEntitiesInText namedEntities = d.getNamedEntitiesInText();
+    final HashMap<NamedEntityInText, String> results = new HashMap<NamedEntityInText, String>();
+    for (final NamedEntityInText namedEntity : namedEntities) {
+      final String disambiguatedURL = namedEntity.getNamedEntityUri();
+      results.put(namedEntity, disambiguatedURL);
+    }
+    for (final NamedEntityInText namedEntity : namedEntities) {
+      final String disambiguatedURL = namedEntity.getNamedEntityUri();
+      System.out.println(namedEntity.getLabel() + " -> " + disambiguatedURL);
+      assertTrue(correct.get(namedEntity.getLabel()).equals(disambiguatedURL));
+    }
+  }
 
-	}
+  @Test
+  public void testContext() throws InterruptedException, IOException {
+    final String angelina = "Angelina";
+    final String angelinaURL = "http://dbpedia.org/resource/Angelina_Jolie";
+    final String brad = "Brad";
+    final String bradURL = "http://dbpedia.org/resource/Brad_Pitt";
+    final String jon = "Jon";
+    final String jonURL = "http://dbpedia.org/resource/Jon_Voight";
 
-	@Test
-	public void testContext() throws InterruptedException, IOException {
-		String angelina = "Angelina";
-		String angelinaURL = "http://dbpedia.org/resource/Angelina_Jolie";
-		String brad = "Brad";
-		String bradURL = "http://dbpedia.org/resource/Brad_Pitt";
-		String jon = "Jon";
-		String jonURL = "http://dbpedia.org/resource/Jon_Voight";
+    final HashMap<String, String> correct = new HashMap<String, String>();
+    correct.put(angelina, angelinaURL);
+    correct.put(jon, jonURL);
+    correct.put(brad, bradURL);
 
-		HashMap<String, String> correct = new HashMap<String, String>();
-		correct.put(angelina, angelinaURL);
-		correct.put(jon, jonURL);
-		correct.put(brad, bradURL);
+    final String preAnnotatedText = "<entity>" + angelina + "</entity>, her father <entity>" + jon
+        + "</entity>, and her partner <entity>" + brad + "</entity> never played together in the same movie.";
 
-		String preAnnotatedText = "<entity>" + angelina + "</entity>, her father <entity>" + jon
-				+ "</entity>, and her partner <entity>" + brad + "</entity> never played together in the same movie.";
+    final NEDAlgo_HITS agdistis = new NEDAlgo_HITS();
+    final Document d = GetDisambiguation.textToDocument(preAnnotatedText);
+    agdistis.run(d, null);
 
-		NEDAlgo_HITS agdistis = new NEDAlgo_HITS();
-		Document d = GetDisambiguation.textToDocument(preAnnotatedText);
-		agdistis.run(d, null);
+    final NamedEntitiesInText namedEntities = d.getNamedEntitiesInText();
+    final HashMap<NamedEntityInText, String> results = new HashMap<NamedEntityInText, String>();
+    for (final NamedEntityInText namedEntity : namedEntities) {
+      final String disambiguatedURL = namedEntity.getNamedEntityUri();
+      results.put(namedEntity, disambiguatedURL);
+    }
+    for (final NamedEntityInText namedEntity : namedEntities) {
+      final String disambiguatedURL = namedEntity.getNamedEntityUri();
+      System.out.println(namedEntity.getLabel() + " -> " + disambiguatedURL);
+      assertTrue(correct.get(namedEntity.getLabel()).equals(disambiguatedURL));
+    }
+  }
 
-		NamedEntitiesInText namedEntities = d.getNamedEntitiesInText();
-		HashMap<NamedEntityInText, String> results = new HashMap<NamedEntityInText, String>();
-		for (NamedEntityInText namedEntity : namedEntities) {
-			String disambiguatedURL = namedEntity.getNamedEntityUri();
-			results.put(namedEntity, disambiguatedURL);
-		}
-		for (NamedEntityInText namedEntity : namedEntities) {
-			String disambiguatedURL = namedEntity.getNamedEntityUri();
-			System.out.println(namedEntity.getLabel() + " -> " + disambiguatedURL);
-			assertTrue(correct.get(namedEntity.getLabel()).equals(disambiguatedURL));
-		}
-	}
+  @Test
+  public void testContext2() throws InterruptedException, IOException {
+    final String e1 = "Jorn Lyseggen";
+    final String dis1 = "http://dbpedia.org/resource/Jørn_Lyseggen";
+    final String e2 = "Chief Executive Officer";
+    final String dis2 = "http://dbpedia.org/resource/CEO";
+    final String e3 = "Meltwater";
+    final String dis3 = "http://dbpedia.org/resource/Meltwater_Group";
+
+    final HashMap<String, String> correct = new HashMap<String, String>();
+    correct.put(e1, dis1);
+    correct.put(e2, dis2);
+    correct.put(e3, dis3);
+
+    final String preAnnotatedText = "<entity>" + e1 + "</entity> is the <entity>" + e2 + "</entity> of <entity>" + e3
+        + "</entity>.";
+
+    final NEDAlgo_HITS agdistis = new NEDAlgo_HITS();
+    final Document d = GetDisambiguation.textToDocument(preAnnotatedText);
+    agdistis.run(d, null);
+
+    final NamedEntitiesInText namedEntities = d.getNamedEntitiesInText();
+    final HashMap<NamedEntityInText, String> results = new HashMap<NamedEntityInText, String>();
+    for (final NamedEntityInText namedEntity : namedEntities) {
+      final String disambiguatedURL = namedEntity.getNamedEntityUri();
+      results.put(namedEntity, disambiguatedURL);
+    }
+    for (final NamedEntityInText namedEntity : namedEntities) {
+      final String disambiguatedURL = namedEntity.getNamedEntityUri();
+      System.out.println(namedEntity.getLabel() + " -> " + disambiguatedURL);
+    }
+  }
 
 }
