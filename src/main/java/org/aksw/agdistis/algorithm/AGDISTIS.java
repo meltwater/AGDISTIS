658ed852e38428f1394d92da8d49c690fb198742
Giorgio <orsi.giorgio@gmail.com>
2018-05-04 22:21:54 +0100

    Added PR score when using PR.

--- src/main/java/org/aksw/agdistis/algorithm/AGDISTIS.java
+++ src/main/java/org/aksw/agdistis/algorithm/AGDISTIS.java
@@ -1,11 +1,7 @@
 package org.aksw.agdistis.algorithm;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import net.logstash.logback.marker.Markers;
 import org.aksw.agdistis.AGDISTISConfiguration;
 import org.aksw.agdistis.Algorithm;
 import org.aksw.agdistis.datatypes.Document;
@@ -21,127 +17,132 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.uci.ics.jung.graph.DirectedSparseGraph;
-import net.logstash.logback.marker.Markers;
+import java.util.*;
 
 public class AGDISTIS {
 
-  private final Logger LOGGER = LoggerFactory.getLogger(AGDISTIS.class);
-  private final String edgeType;
-  private final String nodeType;
-  private final CandidateUtil cu;
-  private final TripleIndex index;
-  private final double threshholdTrigram;
-  private final int maxDepth;
-  private final DomainWhiteLister postDisambiguationDomainWhiteLister;
-  private final boolean heuristicExpansionOn;
-  private final boolean useSurfaceForms;
-  private final Algorithm algorithm;
+    private final Logger LOGGER = LoggerFactory.getLogger(AGDISTIS.class);
+    private final String edgeType;
+    private final String nodeType;
+    private final CandidateUtil cu;
+    private final TripleIndex index;
+    private final double threshholdTrigram;
+    private final int maxDepth;
+    private final DomainWhiteLister postDisambiguationDomainWhiteLister;
+    private final boolean heuristicExpansionOn;
+    private final boolean useSurfaceForms;
+    private final Algorithm algorithm;
 
-  public AGDISTIS() {
-    heuristicExpansionOn = AGDISTISConfiguration.INSTANCE.getHeuristicExpansion();
-    algorithm = AGDISTISConfiguration.INSTANCE.getAlgorithm();
+    public AGDISTIS() {
+        heuristicExpansionOn = AGDISTISConfiguration.INSTANCE.getHeuristicExpansion();
+        algorithm = AGDISTISConfiguration.INSTANCE.getAlgorithm();
 
-    nodeType = AGDISTISConfiguration.INSTANCE.getNodeType().toString();
-    edgeType = AGDISTISConfiguration.INSTANCE.getEdgeType().toString();
-    threshholdTrigram = AGDISTISConfiguration.INSTANCE.getCandidatePruningThreshold();
-    maxDepth = AGDISTISConfiguration.INSTANCE.getSemanticDepth();
-    useSurfaceForms = AGDISTISConfiguration.INSTANCE.getUseSurfaceForms();
-    cu = new CandidateUtil();
-    index = cu.getIndex();
+        nodeType = AGDISTISConfiguration.INSTANCE.getNodeType().toString();
+        edgeType = AGDISTISConfiguration.INSTANCE.getEdgeType().toString();
+        threshholdTrigram = AGDISTISConfiguration.INSTANCE.getCandidatePruningThreshold();
+        maxDepth = AGDISTISConfiguration.INSTANCE.getSemanticDepth();
+        useSurfaceForms = AGDISTISConfiguration.INSTANCE.getUseSurfaceForms();
+        cu = new CandidateUtil();
+        index = cu.getIndex();
 
-    postDisambiguationDomainWhiteLister = new DomainWhiteLister(index,
-        AGDISTISConfiguration.INSTANCE.getPostDisambiguationWhiteListPath());
-  }
+        postDisambiguationDomainWhiteLister = new DomainWhiteLister(index, AGDISTISConfiguration.INSTANCE
+                .getPostDisambiguationWhiteListPath());
+    }
 
-  public void run(final Document document, final Map<NamedEntityInText, List<CandidatesScore>> candidatesPerNE) {
-    try {
+    public void run(final Document document, final Map<NamedEntityInText, List<CandidatesScore>> candidatesPerNE) {
+        try {
 
-      final NamedEntitiesInText namedEntities = document.getNamedEntitiesInText();
-      final DirectedSparseGraph<Node, String> graph = new DirectedSparseGraph<Node, String>();
+            final NamedEntitiesInText namedEntities = document.getNamedEntitiesInText();
+            final DirectedSparseGraph<Node, String> graph = new DirectedSparseGraph<Node, String>();
 
-      LOGGER.debug("Selecting candidates.");
-      long start = System.currentTimeMillis();
-      cu.insertCandidatesIntoText(graph, document, threshholdTrigram, heuristicExpansionOn, useSurfaceForms);
-      final long candidateSelectionTime = System.currentTimeMillis() - start;
+            LOGGER.debug("Selecting candidates.");
+            long start = System.currentTimeMillis();
+            cu.insertCandidatesIntoText(graph, document, threshholdTrigram, heuristicExpansionOn, useSurfaceForms);
+            final long candidateSelectionTime = System.currentTimeMillis() - start;
 
-      LOGGER.debug("Performing graph-based disambiguation.");
-      start = System.currentTimeMillis();
-      // 1) let spread activation/ breadth first search run
-      LOGGER.trace("Graph size before BFS: " + graph.getVertexCount());
-      final BreadthFirstSearch bfs = new BreadthFirstSearch(index, algorithm);
-      bfs.run(maxDepth, graph, edgeType, nodeType);
-      LOGGER.trace("Graph size after BFS: " + graph.getVertexCount());
+            LOGGER.debug("Performing graph-based disambiguation.");
+            start = System.currentTimeMillis();
+            // 1) let spread activation/ breadth first search run
+            LOGGER.trace("Graph size before BFS: " + graph.getVertexCount());
+            final BreadthFirstSearch bfs = new BreadthFirstSearch(index, algorithm);
+            bfs.run(maxDepth, graph, edgeType, nodeType);
+            LOGGER.trace("Graph size after BFS: " + graph.getVertexCount());
 
-      if (algorithm == Algorithm.HITS) {
-        // 2.1) let HITS run
-        LOGGER.debug("Run HITS");
-        final HITS h = new HITS();
-        h.runHits(graph, 20);
-      } else if (algorithm == Algorithm.PAGERANK) {
-        // 2.2) let Pagerank run
-        LOGGER.debug("Run PAGERANK");
-        final PageRank pr = new PageRank();
-        pr.runPr(graph, 50, 0.1);
-      }
-
-      // 3) store the candidate with the highest hub, highest authority
-      // ratio
-      // manipulate which value to use directly in node.compareTo
-      LOGGER.trace("Sort results");
-      final ArrayList<Node> orderedList = new ArrayList<Node>();
-      orderedList.addAll(graph.getVertices());
-      Collections.sort(orderedList);
-      for (final NamedEntityInText entity : namedEntities) {
-        for (final Node m : orderedList) {
-          // there can be one node (candidate) for two labels
-          if (m.containsId(entity.getStartPos()) && postDisambiguationDomainWhiteLister
-              .fitsIntoDomain(m.getCandidateURI(), Optional.of(entity.getType()))) {
-            final String candidateURI = m.getCandidateURI();
-            entity.setNamedEntity(candidateURI);
-            entity.setDisambiguatedTypes(cu.getDisambiguatedTypes(m.getCandidateURI(), index));
-            entity.setAuthorityWeight(m.getAuthorityWeight());
-            entity.setHubWeight(m.getHubWeight());
-            break;
-          }
-        }
-      }
-      // To get all candidates along with their scores
-      if (candidatesPerNE != null) {
-        final List<CandidatesScore> listCandidates = new ArrayList<>();
-        for (final NamedEntityInText entity : namedEntities) {
-          for (int i = 0; i < orderedList.size(); i++) {
-            final Node m = orderedList.get(i);
-
-            // there can be one node (candidate) for two labels
-            if (m.containsId(entity.getStartPos())) {
-
-              final CandidatesScore candidates = new CandidatesScore();
-              candidates.setStart(entity.getStartPos());
-              candidates.setUri(m.getCandidateURI());
-              candidates.setScore(m.getAuthorityWeight());
-              listCandidates.add(candidates);
+            if (algorithm == Algorithm.HITS) {
+                // 2.1) let HITS run
+                LOGGER.debug("Run HITS");
+                final HITS h = new HITS();
+                h.runHits(graph, 20);
+            }
+            else if (algorithm == Algorithm.PAGERANK) {
+                // 2.2) let Pagerank run
+                LOGGER.debug("Run PAGERANK");
+                final PageRank pr = new PageRank();
+                pr.runPr(graph, 50, 0.1);
             }
 
-          }
-          candidatesPerNE.put(entity, listCandidates);
+            // 3) store the candidate with the highest hub, highest authority
+            // ratio
+            // manipulate which value to use directly in node.compareTo
+            LOGGER.trace("Sort results");
+            final ArrayList<Node> orderedList = new ArrayList<Node>();
+            orderedList.addAll(graph.getVertices());
+            Collections.sort(orderedList);
+            for (final NamedEntityInText entity : namedEntities) {
+                for (final Node m : orderedList) {
+                    // there can be one node (candidate) for two labels
+                    if (m.containsId(entity.getStartPos()) && postDisambiguationDomainWhiteLister.fitsIntoDomain(m
+                            .getCandidateURI(), Optional.of(entity.getType()))) {
+                        final String candidateURI = m.getCandidateURI();
+                        entity.setNamedEntity(candidateURI);
+                        entity.setDisambiguatedTypes(cu.getDisambiguatedTypes(m.getCandidateURI(), index));
+                        if (algorithm == Algorithm.HITS) {
+                            entity.setAuthorityWeight(m.getUnnormalizedAuthorityWeight());
+                            entity.setHubWeight(m.getUnnormalizedHubWeight());
+                        }
+                        else if (algorithm == Algorithm.PAGERANK) {
+                            entity.setAuthorityWeight(m.getPageRank());
+                        }
+                        break;
+                    }
+                }
+            }
+            // To get all candidates along with their scores
+            if (candidatesPerNE != null) {
+                final List<CandidatesScore> listCandidates = new ArrayList<>();
+                for (final NamedEntityInText entity : namedEntities) {
+                    for (int i = 0; i < orderedList.size(); i++) {
+                        final Node m = orderedList.get(i);
+
+                        // there can be one node (candidate) for two labels
+                        if (m.containsId(entity.getStartPos())) {
+
+                            final CandidatesScore candidates = new CandidatesScore();
+                            candidates.setStart(entity.getStartPos());
+                            candidates.setUri(m.getCandidateURI());
+                            candidates.setScore(m.getAuthorityWeight());
+                            listCandidates.add(candidates);
+                        }
+
+                    }
+                    candidatesPerNE.put(entity, listCandidates);
+                }
+            }
+            final long disambiguationTime = System.currentTimeMillis() - start;
+            document.setDisambiguationTime(disambiguationTime);
+            document.setCandidateSelectionTime(candidateSelectionTime);
+            document.setAGDISTISVersion(AGDISTISConfiguration.INSTANCE.getAGDISTISVersion());
+
+            LOGGER.debug("Candidates computed in {} msecs", candidateSelectionTime);
+            LOGGER.debug("Disambiguation completed in {} msecs.", disambiguationTime);
+
+        } catch (final Exception e) {
+            LOGGER.error(Markers.append("docId", document.getDocumentId()), "AGDISTIS cannot be run on this document"
+                    + ".", ExceptionUtils.getStackTrace(e));
         }
-      }
-      final long disambiguationTime = System.currentTimeMillis() - start;
-      document.setDisambiguationTime(disambiguationTime);
-      document.setCandidateSelectionTime(candidateSelectionTime);
-      document.setAGDISTISVersion(AGDISTISConfiguration.INSTANCE.getAGDISTISVersion());
-
-      LOGGER.debug("Candidates computed in {} msecs", candidateSelectionTime);
-      LOGGER.debug("Disambiguation completed in {} msecs.", disambiguationTime);
-
-    } catch (final Exception e) {
-      LOGGER.error(Markers.append("docId", document.getDocumentId()), "AGDISTIS cannot be run on this document.",
-          ExceptionUtils.getStackTrace(e));
     }
-  }
 
-  public TripleIndex getIndex() {
-    return cu.getIndex();
-  }
+    public TripleIndex getIndex() {
+        return cu.getIndex();
+    }
 }
